0fc31ef4bcdfe1372e005567815f373d7e9a467e
Josselin Le Gal La Salle <josselin.le-gal-la-salle@univ-reunion.fr>
2026-04-24 14:46:44 +0400

    updating research initialisation

--- ERMESS_Research.py
+++ ERMESS_Research.py
@@ -37,7 +37,7 @@ from ERMESS_scripts.evolutionnary_core import ERMESS_parallel_processing as ppGA
 
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-def ERMESS_research(input_file_path = None,initialisation = False) :
+def ERMESS_research(input_file_path = None,initialisation = False, target_file = None) :
     
     freeze_support()
    
@@ -59,89 +59,80 @@ def ERMESS_research(input_file_path = None,initialisation = False) :
     Context=Dbl.build_environment(structured_data)
         
     if (initialisation) : 
-        Initial_population = Eri.Initialisation_ERMESS_research(Context, structured_data)
+        Eri.Initialize_ERMESS_research(Context , structured_data, target_file)
     
     ##### A utiliser seulement sur le calculateur UNIX
-    set_start_method('spawn')
+#    set_start_method('spawn')
     
     #Avec pré-traitement
     ##print('Début de l\'algorithme évolutif principal')
     
-    num_node = sys.argv[1]
-    cost_phase = int(sys.argv[2])
+#    num_node = sys.argv[1]
+#    cost_phase = int(sys.argv[2])
     
-    Contexte=Dcl.Non_JIT_Environnement(storage_characteristics, time_resolution, n_store, duration_years, specs_num, groups, n_bits, prices_num, fixed_premium, Overrun, Selling_price, Non_movable_load,Y_movable_load,D_movable_load, Main_grid_emissions, Grid_Fossil_fuel_ratio,Main_grid_PoF_ratio, prod_C, prods_U, Volums_prod,Bounds_prod,DG_fuel_cost,DG_lifetime,DG_unit_cost,DG_maintenance_cost,DG_fuel_consumption,DG_EROI,fuel_CO2eq_emissions, constraint_num, Constraint_level, criterion_num, cost_constraint,r_cross,n_iter,hyperparameters_operators_num,type_optim,Connexion,Defined_items,tracking_ope=1)    
+#    Contexte=Dcl.Non_JIT_Environnement(storage_characteristics, time_resolution, n_store, duration_years, specs_num, groups, n_bits, prices_num, fixed_premium, Overrun, Selling_price, Non_movable_load,Y_movable_load,D_movable_load, Main_grid_emissions, Grid_Fossil_fuel_ratio,Main_grid_PoF_ratio, prod_C, prods_U, Volums_prod,Bounds_prod,DG_fuel_cost,DG_lifetime,DG_unit_cost,DG_maintenance_cost,DG_fuel_consumption,DG_EROI,fuel_CO2eq_emissions, constraint_num, Constraint_level, criterion_num, cost_constraint,r_cross,n_iter,hyperparameters_operators_num,type_optim,Connexion,Defined_items,tracking_ope=1)    
 
-    Initial_populations = []
+#    Initial_populations = []
     
-    t2=time.time()
-    print('fin pre-processing ',t2-t1)
+#    t2=time.time()
+#    print('fin pre-processing ',t2-t1)
     
 
-    for file_number in range(n_nodes):
-        with open('pop_'+str(file_number)+'_for_'+str(num_node)+'.dat', 'rb') as input_file:
-            added_pop =pickle.load(input_file)
-            Initial_populations.append(added_pop)
+#    for file_number in range(n_nodes):
+#        with open('pop_'+str(file_number)+'_for_'+str(num_node)+'.dat', 'rb') as input_file:
+#            added_pop =pickle.load(input_file)
+#            Initial_populations.append(added_pop)
     
-    Initial_populations=[x for xs in Initial_populations for x in xs]
+#    Initial_populations=[x for xs in Initial_populations for x in xs]
         
     #Initial_populations = ECl.jitting_pop_res(Initial_populations)
-    Initial_populations = sorted(Initial_populations, key=lambda x: np.random.rand())
-    Initial_populations = [[Initial_populations[i] for i in range(j*n_pop, (j+1)*n_pop)] for j in range(n_core)] 
-    args1_2 = [(Contexte,Initial_populations[i]) for i in range(n_core)]  
+#    Initial_populations = sorted(Initial_populations, key=lambda x: np.random.rand())
+#    Initial_populations = [[Initial_populations[i] for i in range(j*n_pop, (j+1)*n_pop)] for j in range(n_core)] 
+#    args1_2 = [(Contexte,Initial_populations[i]) for i in range(n_core)]  
     
-    t3=time.time()    
-    operators=[]
-    print('populations récupérées ',t3-t2)
-    for ere in range(nb_ere):
-        print('Ere ',ere+1,'/',nb_ere,' ',time.time())
-        print('Evolution indépendante des populations sur chacune des îles sur ',n_iter,' générations')
-        t4=time.time()
-        local_populations_1=ppGA.ere_evolutive(args1_2, ere,nb_ere)
+#    t3=time.time()    
+#    operators=[]
+#    print('populations récupérées ',t3-t2)
+#    for ere in range(nb_ere):
+#        print('Ere ',ere+1,'/',nb_ere,' ',time.time())
+#        print('Evolution indépendante des populations sur chacune des îles sur ',n_iter,' générations')
+#        t4=time.time()
+#        local_populations_1=ppGA.ere_evolutive(args1_2, ere,nb_ere)
     
-        world_population1 = [item for sublist in local_populations_1 for item in sublist[0]]
-        operators.append([item for sublist in local_populations_1 for item in sublist[1]])
-        #world_scores1 = [item for sublist in scores1 for item in sublist]
-        #world_population1=[world_population1[i] for i in np.argsort(scores1)[0:(n_pop*n_core)]]
+#        world_population1 = [item for sublist in local_populations_1 for item in sublist[0]]
+#        operators.append([item for sublist in local_populations_1 for item in sublist[1]])
 
-        world_mix = [world_population1[i] for i in np.random.choice(range(n_pop*n_core),n_pop*n_core,replace=False)]
-        init_population_2 = [world_mix[(island*n_pop):((island+1)*n_pop)] for island in range(n_core)]
+#        world_mix = [world_population1[i] for i in np.random.choice(range(n_pop*n_core),n_pop*n_core,replace=False)]
+#        init_population_2 = [world_mix[(island*n_pop):((island+1)*n_pop)] for island in range(n_core)]
         
-        #file_name_out = 'output_GEMS_'+str(ere)+'.xlsx'
-        #fGA2.post_traitement(world_population1[np.argmin(tuple(world_population1[i].fitness for i in range(len(world_population1))))],Cfc.cost_scenario_LCOE_detail, Cfc.cost_base, prod_C, prods_U, Non_movable_load,D_movable_load,Y_movable_load, storage_characteristics, time_resolution, Main_grid_emissions,Grid_Fossil_fuel_ratio,Main_grid_ratio,specs,duration_years,grid_prices,fixed_premium,Overrun,Selling_price,Contract_Id,Bounds_prod,self_sufficiency,Data,n_days,file_name_out)
-        #print('Mélange aléatoire des populations résultantes')
-        print('Score du meilleur individu actuel : ',min(tuple(world_population1[i].fitness for i in range(len(world_population1)))))
-        args1_2 = [(Contexte,init_population_2[i],n_iter,r_cross) for i in range(n_core)]
-        t5=time.time()
-        print('Ere ',t5-t4)
+#        print('Score du meilleur individu actuel : ',min(tuple(world_population1[i].fitness for i in range(len(world_population1)))))
+#        args1_2 = [(Contexte,init_population_2[i],n_iter,r_cross) for i in range(n_core)]
+#        t5=time.time()
+#        print('Ere ',t5-t4)
         
-    print('Fin de l\'algorithme évolutif ',time.time())
-    world_population3 = [item for sublist in local_populations_1 for item in sublist[0]]
-    scores3 = tuple(world_population3[i].fitness for i in range(len(world_population3)))
-    shuffled_population = sorted(world_population3, key=lambda x: np.random.rand())
+#    print('Fin de l\'algorithme évolutif ',time.time())
+#    world_population3 = [item for sublist in local_populations_1 for item in sublist[0]]
+#    scores3 = tuple(world_population3[i].fitness for i in range(len(world_population3)))
+#    shuffled_population = sorted(world_population3, key=lambda x: np.random.rand())
         
-    for file_number in range(n_nodes):
-        os.remove("pop_"+str(num_node)+'_for_'+str(file_number)+".dat")
-        with open("pop_"+str(num_node)+'_for_'+str(file_number)+".dat", "wb") as f:
-                #for indiv in shuffled_population[int(j*n_pop*n_core/10):int((j+1)*n_pop*n_core/10)]:
-                pickle.dump(shuffled_population[int(file_number*n_pop*n_core/n_nodes):int((file_number+1)*n_pop*n_core/n_nodes)], f)    
+#    for file_number in range(n_nodes):
+#        os.remove("pop_"+str(num_node)+'_for_'+str(file_number)+".dat")
+#        with open("pop_"+str(num_node)+'_for_'+str(file_number)+".dat", "wb") as f:
+#                pickle.dump(shuffled_population[int(file_number*n_pop*n_core/n_nodes):int((file_number+1)*n_pop*n_core/n_nodes)], f)    
     
-    try:
-        os.remove("operators_"+str(num_node)+".dat")
-    except OSError:
-        pass
+#    try:
+#        os.remove("operators_"+str(num_node)+".dat")
+#    except OSError:
+#        pass
     
-    with open("operators_"+str(num_node)+".dat", "wb") as f:
-        pickle.dump(operators, f)
+#    with open("operators_"+str(num_node)+".dat", "wb") as f:
+#        pickle.dump(operators, f)
 
-    t6=time.time()
-    print('fin ',t6-t5)    
-    print('temps total : ',t6-t1)      
-    #GA_solution = world_population3[np.argmin(scores3)]
+#    t6=time.time()
+#    print('fin ',t6-t5)    
+#    print('temps total : ',t6-t1)      
 
-    #file_name_out='output_GEMS_end.xlsx'
-    #fGA2.post_traitement(GA_solution,Cfc.cost_scenario_LCOE_detail, Cfc.cost_base, prod_C, prods_U, Non_movable_load,D_movable_load,Y_movable_load, storage_characteristics, time_resolution, Main_grid_emissions,Grid_Fossil_fuel_ratio,Main_grid_ratio,specs,duration_years,grid_prices,fixed_premium,Overrun,Selling_price,Contract_Id,Bounds_prod,self_sufficiency,Data,n_days,file_name_out)
 
 if __name__ == '__main__':
-    ERMESS_research(sys.argv[1])
+    ERMESS_research(sys.argv[1],True,sys.argv[2])
 
