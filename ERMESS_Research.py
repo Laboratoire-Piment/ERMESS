1acfb44e61514dab841607633f80ef7cc373466c
Josselin Le Gal La Salle <josselin.le-gal-la-salle@univ-reunion.fr>
2026-05-06 10:31:59 +0400

    updating ERMESS research pipe

--- ERMESS_Research.py
+++ ERMESS_Research.py
@@ -230,29 +230,32 @@ def run_ERMESS_research(Context, nb_ere, n_core, node_id, n_nodes):
         # -----------------------
         if ere % INTERVAL_EXCHANGE == 0:
 
-#            migrant_internodes = select_migrants_internodes(len_pop, MIGRATION_TOP_RATE, MIGRATION_RANDOM_RATE,n_core)
-#            local_migrants = [[local_populations[i][j] for j in migrant_internodes] for i in range(n_core)]
-#            migrants = [ item for sublist in local_migrants for item in sublist ]
-#            print('migrants ', type(migrants),len(migrants),type(migrants[0]))
+            migrant_internodes = select_migrants_internodes(len_pop, MIGRATION_TOP_RATE, MIGRATION_RANDOM_RATE,n_core)
+            local_migrants = [[local_populations[i][j] for j in migrant_internodes] for i in range(n_core)]
+            migrants = [ item for sublist in local_migrants for item in sublist ]
+            print('migrants ', type(migrants),len(migrants),type(migrants[0]))
 
-#            write_migrants(migrants, node_id, ere)
+            write_migrants(migrants, node_id, ere)
 
-#            files = wait_for_all(ere, n_nodes)
-#            potential_incomers = load_migrants(files)
+            files = wait_for_all(ere, n_nodes)
+            potential_incomers = load_migrants(files)
 
-#            len_incomers = int((MIGRATION_TOP_RATE+MIGRATION_RANDOM_RATE)*len_pop*n_core)
-#            incomers = collect_migrants(potential_incomers,len_incomers)
-#            print('incomers ', type(incomers),len(incomers))
+            len_incomers = int((MIGRATION_TOP_RATE+MIGRATION_RANDOM_RATE)*len_pop*n_core)
+            incomers = collect_migrants(potential_incomers,len_incomers)
+            print('incomers ', type(incomers),len(incomers))
+            for d in range(len(incomers)) :
+                if type(incomers[d])==list:
+                    print(d,incomers[d])
 
-#            killed_indices = select_replaced_internodes(len_pop, MIGRATION_TOP_RATE, len_incomers,n_core)           
+            killed_indices = select_replaced_internodes(len_pop, MIGRATION_TOP_RATE, len_incomers,n_core)           
             
-#            local_populations = replace_population_internodes (n_core,len_pop,local_populations,incomers,killed_indices,MIGRATION_TOP_RATE,MIGRATION_RANDOM_RATE)
+            local_populations = replace_population_internodes (n_core,len_pop,local_populations,incomers,killed_indices,MIGRATION_TOP_RATE,MIGRATION_RANDOM_RATE)
             print('after migration ', type(local_populations),len(local_populations),type(local_populations[0]),len(local_populations[0]))
 
         
         Initial_populations = local_populations
     
-    node_population = [ item for sublist in local_populations for item in sublist[0] ]
+    node_population = [ item for sublist in local_populations for item in sublist ]
     best_score = min(ind.fitness for ind in node_population)
     print('best score : ',best_score)
     Eri.write_node_population(node_id,node_population)
